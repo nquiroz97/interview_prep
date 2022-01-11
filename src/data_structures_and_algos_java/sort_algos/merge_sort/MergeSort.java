38d7cb4ca619bd2bc5a5590caff8e46e9f5c7f5e
Neri Quiroz <68469197+nquiroz97@users.noreply.github.com>
2022-01-11 15:09:54 -0500

    Quick Sort

--- src/data_structures_and_algos_java/sort_algos/merge_sort/MergeSort.java
+++ src/data_structures_and_algos_java/sort_algos/merge_sort/MergeSort.java
@@ -24,7 +24,8 @@ public class MergeSort {
         //merges right array
         mergeSort(array, mid, end);
         //merge left and right partitions
-        merge(array, start, mid, end);
+        //merge(array, start, mid, end);
+        mergeDescending(array, start, mid, end);
     }
 
     private static void merge(int[] array, int start, int mid, int end) {
@@ -42,6 +43,27 @@ public class MergeSort {
         int[] temp = new int[end -start];
         while(i < mid && j < end){
             temp[tempIndex++] = array[i] <= array[j] ? array[i++] : array[j++];
+        }
+        System.arraycopy(array, i, array, start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, array, start, tempIndex);
+    }
+
+    //Challenge 1: modify to sort in desc order
+    private static void mergeDescending(int[] array, int start, int mid, int end) {
+        /* right partition is already sorted, so if right elements are greater than first element
+           oright partition, all elements of left are less than first element of right. and 
+           all other elements of right partition are greater than first element of right */
+        if(array[mid - 1] >= array[mid]){
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        int[] temp = new int[end -start];
+        while(i < mid && j < end){
+            temp[tempIndex++] = array[i] >= array[j] ? array[i++] : array[j++];
         }
         System.arraycopy(array, i, array, start + tempIndex, mid - i);
         System.arraycopy(temp, 0, array, start, tempIndex);
--- src/data_structures_and_algos_java/sort_algos/quick_sort/QuickSort.java
+++ src/data_structures_and_algos_java/sort_algos/quick_sort/QuickSort.java
@@ -4,6 +4,8 @@ public class QuickSort {
     public static void main(String[] args) {
         int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
+        quickSort(intArray, 0, intArray.length);
+
         for(int i = 0; i < intArray.length; i++){
             System.out.println(intArray[i]);
         }
